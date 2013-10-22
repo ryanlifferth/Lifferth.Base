61d04762c398d4cf46691f7792cf75054bbd5ed9
Ryan Lifferth <ryanlifferth@gmail.com>
2013-10-21 20:20:17 -0600

    New project added

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lifferth.Base.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Lifferth.Base
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // Needed to allow for PartialViews subdirectory
+            ViewEngines.Engines.Add(new RazorViewEngine
+            {
+                PartialViewLocationFormats = new string[]
+                {
+                    "~/Views/{1}/PartialViews/{0}.cshtml",
+                    "~/Views/Shared/PartialViews/{0}.cshtml"
+                }
+            });
+        }
+    }
+}
